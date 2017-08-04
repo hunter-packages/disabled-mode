2e4a756e12786c0c0333be58b8d8b986375ca326
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-08-04 15:26:45 +0300

    Add CURLConfig.cmake

---
+++ CURLConfig.cmake
@@ -0,0 +1,14 @@
+if(TARGET CURL::libcurl)
+  return()
+endif()
+
+find_package(CURL MODULE REQUIRED)
+
+add_library(CURL::libcurl UNKNOWN IMPORTED)
+
+set_target_properties(
+    CURL::libcurl
+    PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}"
+    IMPORTED_LOCATION "${CURL_LIBRARIES}"
+)
